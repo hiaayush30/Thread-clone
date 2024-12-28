e9290ff0683ceeb6da32cee2a485a064d72ca902
aayush <hiaayush30@gmail.com>
2024-12-28 14:01:47 +0530

    added get all posts and delete post route

--- Server/src/controllers/post.js
+++ Server/src/controllers/post.js
@@ -1,8 +1,9 @@
 const cloudinary = require('../config/cloudinary');
 const formidable = require('formidable');
-const { INTERNAL_SERVER_ERROR, BAD_REQUEST, CREATED } = require('../constants/statusCodes');
+const { INTERNAL_SERVER_ERROR, BAD_REQUEST, CREATED, OK, FORBIDDEN } = require('../constants/statusCodes');
 const { UserModel } = require('../modals/User');
 const { PostModel } = require('../modals/Post');
+const { CommentModel } = require('../modals/Comment');
 
 const addPost = async (req, res) => {
     try {
@@ -10,18 +11,23 @@ const addPost = async (req, res) => {
         let uploadedImage;
         form.parse(req, async (err, feilds, files) => {
             if (err) {
-                console.log('formidable parsing error:'+err);
+                console.log('formidable parsing error:' + err);
                 res.status(INTERNAL_SERVER_ERROR).json({
-                    message:'internal server error in file parsing'
+                    message: 'internal server error in file parsing'
                 })
             }
             if (!feilds.text) return res.status(BAD_REQUEST).json({
                 message: 'text feild cannot be empty'
             })
             if (files.media) {
-                uploadedImage = await cloudinary.uploader.upload(files.media.filepath,{
-                    folder:'Threads_clone/Posts'
+                uploadedImage = await cloudinary.uploader.upload(files.media.filepath, {
+                    folder: 'Threads_clone/Posts'
                 });
+                if (!uploadedImage) {
+                    res.status(INTERNAL_SERVER_ERROR).json({
+                        message: "Error while uploading image"
+                    })
+                }
             }
             const post = await PostModel.create({
                 admin: req.user._id,
@@ -31,11 +37,11 @@ const addPost = async (req, res) => {
                     public_id: uploadedImage.public_id
                 })
             })
-            await UserModel.findByIdAndUpdate(req.user._id,{
-                $push:{threads:post._id}
+            await UserModel.findByIdAndUpdate(req.user._id, {
+                $push: { threads: post._id }
             })
             res.status(CREATED).json({
-                message:'post added!'
+                message: 'post added!'
             })
         })
     } catch (err) {
@@ -47,6 +53,75 @@ const addPost = async (req, res) => {
     }
 }
 
+const getAllPosts = async (req, res) => {
+    try {
+        let { page, limit } = req.query;
+        if (!limit || limit > 5 || limit < 1 || limit === undefined) limit = 5;
+        if (!page || page < 1 || page === undefined) page = 1;
+        const posts = await PostModel.find({})
+            .sort({ createdAt: -1 })
+            //descending order ie the latest at the top  
+            .skip((page - 1) * limit)
+            .limit(limit)
+            .populate('likes')
+            .populate('admin')
+            .populate({ path: 'comments', populate: 'admin' })  //to get the names of those who commented
+        res.status(OK).json({
+            message: 'posts fetched successfully',
+            posts
+        })
+    } catch (err) {
+        console.log('getAllPost error:' + err);
+        res.status(INTERNAL_SERVER_ERROR).json({
+            message: 'could not fetch posts'
+        })
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const post = await PostModel.findById({_id:postId});
+        if(!post) return res.status(BAD_REQUEST).json({
+            message:'post not found'
+        });
+       if(post.admin.toString() !== req.user._id.toString()){
+        return res.status(FORBIDDEN).json({
+            message:'cannot delete post of another user'
+        })
+       }
+       //all cehcks done
+       if(post.media){
+        await cloudinary.uploader.destroy(post.public_id,{},(err,result)=>{
+            console.log({err,result});
+        });
+       }
+       await UserModel.updateMany({ 
+        $or:[{threads:postId},{replies:postId},{reposts:postId}] //search for these docs
+       },{
+        $pull:{  //delete these
+            replies:postId,
+            reposts:postId,
+            threads:postId
+        }
+       })
+       await CommentModel.deleteMany({
+        post:postId
+       });
+       await PostModel.findByIdAndDelete(postId);
+       res.status(OK).json({
+        message:'post deleted'
+       })
+    } catch (err) {
+        console.log('delete post err' + err);
+        res.status(INTERNAL_SERVER_ERROR).json({
+            message: 'internal server error while deleting post'
+        })
+    }
+}
+
 module.exports = {
-    addPost
+    addPost,
+    getAllPosts,
+    deletePost
 }
--- Server/src/routes/post.js
+++ Server/src/routes/post.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { addPost } = require('../controllers/post');
+const { addPost, getAllPosts, deletePost } = require('../controllers/post');
 const { userAuth } = require('../middlewares/auth');
 const postRouter=express.Router();
 
 postRouter.post('/add',userAuth,addPost);
+postRouter.get('/',userAuth,getAllPosts);
+postRouter.delete('/:postId',userAuth,deletePost);
 
 module.exports={postRouter};
