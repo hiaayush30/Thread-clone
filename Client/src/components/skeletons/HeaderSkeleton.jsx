df111ab45bff81e214ddc791230bacd476b6779e
aayush <hiaayush30@gmail.com>
2025-03-17 12:22:41 +0530

    updated ui

--- Client/src/components/skeletons/HeaderSkeleton.jsx
+++ Client/src/components/skeletons/HeaderSkeleton.jsx
@@ -10,16 +10,6 @@ function HeaderSkeleton() {
                 <div className="relative w-12 h-12">
                 </div>
             </div>
-            <div className="md:hidden animate-pulse">
-                <div className='flex h-20 justify-around items-center sticky top-5 py-1'>
-                    <div className="w-12 h-12"></div>
-                    <div className="absolute right-5 w-12 h-12">
-                    </div>
-                </div>
-                <div className="dark:bg-zinc-800  fixed bottom-0 bg-blue-100 right-0 left-0 p-1 z-20 pt-2 opacity-95">
-                   
-                </div>
-            </div>
         </>
     )
 }
